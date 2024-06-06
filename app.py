3bad1aaa3c4be02c47d609e22a82d55ec3ba3ff9
akeenantexasfcs <akeenan@texasfcs.com>
2024-06-06 13:18:33 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
+# In[9]:
 
 
 import io
@@ -494,7 +494,7 @@ def balance_sheet():
         uploaded_dict_file = st.file_uploader("Upload a new Data Dictionary CSV", type=['csv'], key='dict_uploader_tab4_bs')
         if uploaded_dict_file is not None:
             new_lookup_df = pd.read_csv(uploaded_dict_file)
-            lookup_df = new_lookup_df  # Overwrite the entire DataFrame
+            lookup_df = pd.concat([lookup_df, new_lookup_df], ignore_index=True).drop_duplicates().reset_index(drop=True)
             save_lookup_table(lookup_df, data_dictionary_file)
             st.success("Data Dictionary uploaded and updated successfully!")
 
@@ -819,12 +819,18 @@ def cash_flow_statement():
                     for idx, row in df.iterrows():
                         manual_selection = row['Manual Selection']
                         final_mnemonic = row['Final Mnemonic Selection']
+                        if manual_selection == 'Other Category':
+                            ciq_value = 'CIQ IQ Required'
+                        else:
+                            ciq_value = cash_flow_lookup_df.loc[cash_flow_lookup_df['Mnemonic'] == final_mnemonic, 'CIQ'].values[0] if not cash_flow_lookup_df.loc[cash_flow_lookup_df['Mnemonic'] == final_mnemonic, 'CIQ'].empty else 'CIQ IQ Required'
+                        
                         if manual_selection not in ['Other Category', 'REMOVE ROW', '']:
                             if row['Account'] not in cash_flow_lookup_df['Account'].values:
-                                new_entries.append({'Account': row['Account'], 'Mnemonic': final_mnemonic, 'CIQ': '', 'Label': row['Label']})
+                                new_entries.append({'Account': row['Account'], 'Mnemonic': final_mnemonic, 'CIQ': ciq_value, 'Label': row['Label']})
                             else:
                                 cash_flow_lookup_df.loc[cash_flow_lookup_df['Account'] == row['Account'], 'Mnemonic'] = final_mnemonic
                                 cash_flow_lookup_df.loc[cash_flow_lookup_df['Account'] == row['Account'], 'Label'] = row['Label']
+                                cash_flow_lookup_df.loc[cash_flow_lookup_df['Account'] == row['Account'], 'CIQ'] = ciq_value
                     if new_entries:
                         cash_flow_lookup_df = pd.concat([cash_flow_lookup_df, pd.DataFrame(new_entries)], ignore_index=True)
                     cash_flow_lookup_df.reset_index(drop=True, inplace=True)
@@ -837,7 +843,7 @@ def cash_flow_statement():
         uploaded_dict_file = st.file_uploader("Upload a new Data Dictionary CSV", type=['csv'], key='dict_uploader_tab4_cfs')
         if uploaded_dict_file is not None:
             new_lookup_df = pd.read_csv(uploaded_dict_file)
-            cash_flow_lookup_df = pd.concat([cash_flow_lookup_df, new_lookup_df], ignore_index=True).drop_duplicates().reset_index(drop=True)
+            cash_flow_lookup_df = new_lookup_df  # Always override with the new uploaded dictionary
             save_lookup_table(cash_flow_lookup_df, cash_flow_data_dictionary_file)
             st.success("Data Dictionary uploaded and updated successfully!")
 
