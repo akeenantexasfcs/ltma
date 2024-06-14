2533dbb22140c9f72314eced298871b25cbb5b02
akeenantexasfcs <akeenan@texasfcs.com>
2024-06-14 17:36:28 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[25]:
+# In[9]:
 
 
 import io
@@ -247,8 +247,8 @@ def balance_sheet():
                 st.dataframe(preview_table)
 
             st.subheader("Rename Columns")
-            quarter_options = [f"Q{i}-{year}" for year in range(2018, 2027) for i in range(1, 5)]
-            ytd_options = [f"YTD {year}" for year in range(2018, 2027)]
+            quarter_options = [f"FQ{quarter}{year}" for year in range(2018, 2027) for quarter in range(1, 5)]
+            ytd_options = [f"YTD{quarter}{year}" for year in range(2018, 2027) for quarter in range(1, 5)]
             dropdown_options = [''] + ['Account'] + quarter_options + ytd_options
 
             for col in all_tables.columns:
@@ -606,8 +606,8 @@ def cash_flow_statement():
                 st.dataframe(preview_table)
 
             st.subheader("Rename Columns")
-            quarter_options = [f"Q{i}-{year}" for year in range(2018, 2027) for i in range(1, 5)]
-            ytd_options = [f"YTD {year}" for year in range(2018, 2027)]
+            quarter_options = [f"FQ{quarter}{year}" for year in range(2018, 2027) for quarter in range(1, 5)]
+            ytd_options = [f"YTD{quarter}{year}" for year in range(2018, 2027) for quarter in range(1, 5)]
             dropdown_options = [''] + ['Account'] + quarter_options + ytd_options
 
             for col in all_tables.columns:
@@ -851,10 +851,18 @@ def cash_flow_statement():
             excel_file.seek(0)
             st.download_button("Download Excel", excel_file, "cash_flow_data_dictionary.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
-# Global variables and functions
+
+######################################INCOME STATEMENT##################################
 # Global variables and functions
 income_statement_lookup_df = pd.DataFrame()
 income_statement_data_dictionary_file = 'income_statement_data_dictionary.xlsx'
+
+conversion_factors = {
+    "Actuals": 1,
+    "Thousands": 1000,
+    "Millions": 1000000,
+    "Billions": 1000000000
+}
 
 def save_lookup_table(df, file_path):
     df.to_excel(file_path, index=False)
@@ -929,6 +937,35 @@ def aggregate_data(files):
 
     return final_df
 
+def create_combined_df(dfs):
+    combined_df = pd.DataFrame()
+    for i, df in enumerate(dfs):
+        final_mnemonic_col = 'Final Mnemonic Selection'
+        if final_mnemonic_col not in df.columns:
+            st.error(f"Column '{final_mnemonic_col}' not found in dataframe {i+1}")
+            continue
+        
+        date_cols = [col for col in df.columns if col not in ['Account', final_mnemonic_col, 'Mnemonic', 'Manual Selection', 'Sort Index']]
+        if not date_cols:
+            st.error(f"No date columns found in dataframe {i+1}")
+            continue
+
+        df_grouped = df.groupby([final_mnemonic_col]).sum(numeric_only=True).reset_index()
+        df_melted = df_grouped.melt(id_vars=[final_mnemonic_col], value_vars=date_cols, var_name='Date', value_name='Value')
+        df_pivot = df_melted.pivot(index=[final_mnemonic_col], columns='Date', values='Value')
+        
+        if combined_df.empty:
+            combined_df = df_pivot
+        else:
+            combined_df = combined_df.join(df_pivot, how='outer')
+    return combined_df.reset_index()
+
+def sort_by_sort_index(df):
+    # Sort by Sort Index if it exists
+    if 'Sort Index' in df.columns:
+        df = df.sort_values(by=['Sort Index'])
+    return df
+
 def income_statement():
     global income_statement_lookup_df
 
@@ -961,7 +998,7 @@ def income_statement():
                                             for rel in cell_block['Relationships']:
                                                 if rel['Type'] == 'CHILD':
                                                     for word_id in rel['Ids']:
-                                                        word_block = next((w for w in data['Blocks'] if b['Id'] == word_id), None)
+                                                        word_block = next((w for w in data['Blocks'] if w['Id'] == word_id), None)
                                                         if word_block and word_block['BlockType'] == 'WORD':
                                                             cell_text += ' ' + word_block.get('Text', '')
                                         table[row_index][col_index] = cell_text.strip()
@@ -1123,43 +1160,164 @@ def income_statement():
     with tab3:
         st.subheader("Mappings and Data Aggregation")
 
-        st.write("This section can be used to define and manage mappings for data aggregation.")
-        st.write("You can upload mappings, define rules, and manage aggregated data here.")
+        uploaded_excel_is = st.file_uploader("Upload your Excel file for Mnemonic Mapping", type=['xlsx'], key='excel_uploader_tab3_is')
 
-        uploaded_mapping_file = st.file_uploader("Upload Mapping File", type=['xlsx', 'csv'], key='mapping_uploader_mda')
-        if uploaded_mapping_file:
-            if uploaded_mapping_file.name.endswith('.xlsx'):
-                mapping_df = pd.read_excel(uploaded_mapping_file)
+        currency_options_is = ["U.S. Dollar", "Euro", "British Pound Sterling", "Japanese Yen"]
+        magnitude_options_is = ["Actuals", "Thousands", "Millions", "Billions", "Trillions"]
+
+        selected_currency_is = st.selectbox("Select Currency", currency_options_is, key='currency_selection_tab3_is')
+        selected_magnitude_is = st.selectbox("Select Magnitude", magnitude_options_is, key='magnitude_selection_tab3_is')
+        company_name_is = st.text_input("Enter Company Name", key='company_name_input_is')
+
+        if uploaded_excel_is is not None:
+            df_is = pd.read_excel(uploaded_excel_is)
+            st.write("Columns in the uploaded file:", df_is.columns.tolist())
+
+            if 'Account' not in df_is.columns:
+                st.error("The uploaded file does not contain an 'Account' column.")
             else:
-                mapping_df = pd.read_csv(uploaded_mapping_file)
+                # Ensure Sort Index is present
+                if 'Sort Index' not in df_is.columns:
+                    df_is['Sort Index'] = range(1, len(df_is) + 1)
 
-            st.write("Uploaded Mapping Data")
-            st.dataframe(mapping_df)
+                # Function to get the best match based on Account using Levenshtein distance
+                def get_best_match_is(account):
+                    best_score_is = float('inf')
+                    best_match_is = None
+                    for _, lookup_row in income_statement_lookup_df.iterrows():
+                        lookup_account = lookup_row['Account']
+                        account_str = str(account)
+                        score_is = levenshtein_distance(account_str.lower(), lookup_account.lower()) / max(len(account_str), len(lookup_account))
+                        if score_is < best_score_is:
+                            best_score_is = score_is
+                            best_match_is = lookup_row
+                    return best_match_is, best_score_is
+
+                df_is['Mnemonic'] = ''
+                df_is['Manual Selection'] = ''
+                for idx, row in df_is.iterrows():
+                    account_value = row['Account']
+                    if pd.notna(account_value):
+                        best_match_is, score_is = get_best_match_is(account_value)
+                        if best_match_is is not None and score_is < 0.25:
+                            df_is.at[idx, 'Mnemonic'] = best_match_is['Mnemonic']
+                        else:
+                            df_is.at[idx, 'Mnemonic'] = 'Human Intervention Required'
+                    
+                    if df_is.at[idx, 'Mnemonic'] == 'Human Intervention Required':
+                        message_is = f"**Human Intervention Required for:** {account_value} - Index {idx}"
+                        st.markdown(message_is)
+                    
+                    manual_selection_is = st.selectbox(
+                        f"Select category for '{account_value}'",
+                        options=[''] + income_statement_lookup_df['Mnemonic'].tolist() + ['REMOVE ROW'],
+                        key=f"select_{idx}_tab3_is"
+                    )
+                    if manual_selection_is:
+                        df_is.at[idx, 'Manual Selection'] = manual_selection_is.strip()
+
+                st.dataframe(df_is[['Account', 'Mnemonic', 'Manual Selection', 'Sort Index']])  # Include 'Sort Index' as a helper column
+
+                if st.button("Generate Excel with Lookup Results", key="generate_excel_lookup_results_tab3_is"):
+                    df_is['Final Mnemonic Selection'] = df_is.apply(
+                        lambda row: row['Manual Selection'].strip() if row['Manual Selection'].strip() != '' else row['Mnemonic'], 
+                        axis=1
+                    )
+                    final_output_df_is = df_is[df_is['Final Mnemonic Selection'].str.strip() != 'REMOVE ROW'].copy()
+                    
+                    combined_df_is = create_combined_df([final_output_df_is])
+                    combined_df_is = sort_by_sort_index(combined_df_is)
+
+                    # Add CIQ column based on lookup
+                    def lookup_ciq_is(mnemonic):
+                        if mnemonic == 'Human Intervention Required':
+                            return 'CIQ IQ Required'
+                        ciq_value_is = income_statement_lookup_df.loc[income_statement_lookup_df['Mnemonic'] == mnemonic, 'CIQ']
+                        if ciq_value_is.empty:
+                            return 'CIQ IQ Required'
+                        return ciq_value_is.values[0]
+                    
+                    combined_df_is['CIQ'] = combined_df_is['Final Mnemonic Selection'].apply(lookup_ciq_is)
+
+                    columns_order_is = ['Final Mnemonic Selection', 'CIQ'] + [col for col in combined_df_is.columns if col not in ['Final Mnemonic Selection', 'CIQ']]
+                    combined_df_is = combined_df_is[columns_order_is]
+
+                    # Include the "As Presented" sheet without the CIQ column, and with the specified column order
+                    as_presented_df_is = final_output_df_is.drop(columns=['CIQ', 'Mnemonic', 'Manual Selection'], errors='ignore')
+                    as_presented_df_is = sort_by_sort_index(as_presented_df_is)
+                    as_presented_df_is = as_presented_df_is.drop(columns=['Sort Index'], errors='ignore')
+                    as_presented_columns_order_is = ['Account', 'Final Mnemonic Selection'] + [col for col in as_presented_df_is.columns if col not in ['Account', 'Final Mnemonic Selection']]
+                    as_presented_df_is = as_presented_df_is[as_presented_columns_order_is]
+
+                    excel_file_is = io.BytesIO()
+                    with pd.ExcelWriter(excel_file_is, engine='xlsxwriter') as writer:
+                        combined_df_is.to_excel(writer, sheet_name='Standardized', index=False)
+                        as_presented_df_is.to_excel(writer, sheet_name='As Presented', index=False)
+                        cover_df_is = pd.DataFrame({
+                            'Selection': ['Currency', 'Magnitude', 'Company Name'],
+                            'Value': [selected_currency_is, selected_magnitude_is, company_name_is]
+                        })
+                        cover_df_is.to_excel(writer, sheet_name='Cover', index=False)
+                    excel_file_is.seek(0)
+                    st.download_button("Download Excel", excel_file_is, "mnemonic_mapping_with_aggregation_is.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+
+                if st.button("Update Data Dictionary with Manual Mappings", key="update_data_dictionary_tab3_is"):
+                    df_is['Final Mnemonic Selection'] = df_is.apply(
+                        lambda row: row['Manual Selection'] if row['Manual Selection'] not in ['REMOVE ROW', ''] else row['Mnemonic'], 
+                        axis=1
+                    )
+                    new_entries_is = []
+                    for idx, row in df_is.iterrows():
+                        manual_selection_is = row['Manual Selection']
+                        final_mnemonic_is = row['Final Mnemonic Selection']
+                        if manual_selection_is == 'REMOVE ROW':
+                            continue
+                        ciq_value_is = income_statement_lookup_df.loc[income_statement_lookup_df['Mnemonic'] == final_mnemonic_is, 'CIQ'].values[0] if not income_statement_lookup_df.loc[income_statement_lookup_df['Mnemonic'] == final_mnemonic_is, 'CIQ'].empty else 'CIQ IQ Required'
+                        
+                        if manual_selection_is not in ['REMOVE ROW', '']:
+                            if row['Account'] not in income_statement_lookup_df['Account'].values:
+                                new_entries_is.append({'Account': row['Account'], 'Mnemonic': final_mnemonic_is, 'CIQ': ciq_value_is})
+                            else:
+                                income_statement_lookup_df.loc[income_statement_lookup_df['Account'] == row['Account'], 'Mnemonic'] = final_mnemonic_is
+                                income_statement_lookup_df.loc[income_statement_lookup_df['Account'] == row['Account'], 'CIQ'] = ciq_value_is
+                    if new_entries_is:
+                        income_statement_lookup_df = pd.concat([income_statement_lookup_df, pd.DataFrame(new_entries_is)], ignore_index=True)
+                    income_statement_lookup_df.reset_index(drop=True, inplace=True)
+                    save_lookup_table(income_statement_lookup_df, income_statement_data_dictionary_file)
+                    st.success("Data Dictionary Updated Successfully")
 
     with tab4:
         st.subheader("Income Statement Data Dictionary")
 
-        uploaded_dict_file = st.file_uploader("Upload a new Data Dictionary CSV", type=['csv'], key='dict_uploader_tab4_is')
-        if uploaded_dict_file is not None:
-            new_lookup_df = pd.read_csv(uploaded_dict_file)
-            income_statement_lookup_df = new_lookup_df  # Overwrite the entire DataFrame
-            save_lookup_table(income_statement_lookup_df, income_statement_data_dictionary_file)
+        # Load most recent CSV in memory until a new CSV is uploaded
+        if 'income_statement_data' not in st.session_state:
+            if os.path.exists(income_statement_data_dictionary_file):
+                st.session_state.income_statement_data = pd.read_excel(income_statement_data_dictionary_file)
+            else:
+                st.session_state.income_statement_data = pd.DataFrame()
+
+        uploaded_dict_file_is = st.file_uploader("Upload a new Data Dictionary CSV", type=['csv'], key='dict_uploader_tab4_is')
+        if uploaded_dict_file_is is not None:
+            new_lookup_df_is = pd.read_csv(uploaded_dict_file_is)
+            st.session_state.income_statement_data = new_lookup_df_is  # Update the session state with new DataFrame
+            save_lookup_table(new_lookup_df_is, income_statement_data_dictionary_file)
             st.success("Data Dictionary uploaded and updated successfully!")
 
-        st.dataframe(income_statement_lookup_df)
+        # Use the data from the session state
+        st.dataframe(st.session_state.income_statement_data)
 
-        remove_indices = st.multiselect("Select rows to remove", income_statement_lookup_df.index, key='remove_indices_tab4_is')
+        remove_indices_is = st.multiselect("Select rows to remove", st.session_state.income_statement_data.index, key='remove_indices_tab4_is')
         if st.button("Remove Selected Rows", key="remove_selected_rows_tab4_is"):
-            income_statement_lookup_df = income_statement_lookup_df.drop(remove_indices).reset_index(drop=True)
-            save_lookup_table(income_statement_lookup_df, income_statement_data_dictionary_file)
+            st.session_state.income_statement_data = st.session_state.income_statement_data.drop(remove_indices_is).reset_index(drop=True)
+            save_lookup_table(st.session_state.income_statement_data, income_statement_data_dictionary_file)
             st.success("Selected rows removed successfully!")
-            st.dataframe(income_statement_lookup_df)
+            st.dataframe(st.session_state.income_statement_data)
 
         if st.button("Download Data Dictionary", key="download_data_dictionary_tab4_is"):
-            excel_file = io.BytesIO()
-            income_statement_lookup_df.to_excel(excel_file, index=False)
-            excel_file.seek(0)
-            st.download_button("Download Excel", excel_file, "income_statement_data_dictionary.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            excel_file_is = io.BytesIO()
+            st.session_state.income_statement_data.to_excel(excel_file_is, index=False)
+            excel_file_is.seek(0)
+            st.download_button("Download Excel", excel_file_is, "income_statement_data_dictionary.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
 def main():
     st.sidebar.title("Navigation")
